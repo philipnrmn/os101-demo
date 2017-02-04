b7542813eddb7c4d1e1d8c9ba7fc81dd8a0a8941
Sunil Shah <sunil@mesosphere.io>
2017-02-04 10:24:49 -0500

    Add example files.

---
+++ appB/main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def api_root():
+    return 'Hi, I\'m appB. Slides at <a href=\"https://mesosphere.github.io/presentations\">https://mesosphere.github.io/presentations</a>'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
